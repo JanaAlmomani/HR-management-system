7f050a3caf58cea0fd28f357d8e699cd92f5c793
JanaAlmomani <jana2000510@gmail.com>
2023-02-14 15:32:53 +0000

    update README file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # HR-management-system
+### lab-08
+### for today lab ,i will do the following depend on the requirements:
+ #### add a form with the following fields:
+
+ 1. Full name
+ 2. Department with following options:
+
+    . Administration
+    . Marketing
+    .  Development
+    .  Finance
+3. Level with following options:
+
+    . Junior
+    .  Mid-Senior
+    . Senior
+
+4. Image url
+
+5. Submit button
+
+### then 
+6. Create a function to generate a unique four digits employee id number.
+
+7.  add an event listener to get the data from the form 
+8. refactor the render prototype function to render each employee information in a separate card in the home page
--- demo/app.js
+++ demo/app.js
@@ -40,8 +40,6 @@ Employee.prototype.render = function(){
     imgEl.width = "150";
     imgEl.height = "150";
 
-    // const spaceEl=document.createTextNode(" ");
-    // divEL.appendChild(spaceEl);
 
     // display emp name 
     const nameEl = document.createElement('p');
@@ -73,6 +71,8 @@ const hrEl = document.createElement('hr');
     divEl.appendChild(hrEl);
 
 }
+
+
 let employeeForm = document.getElementById("employeeForm");
 employeeForm.addEventListener('submit', addNewEepHandler);
 
@@ -92,33 +92,3 @@ function addNewEepHandler(event) {
 
 
 }
-
-
-
-// function generateUniqueId(){
-// return Math.floor(Math.random() * 9000 + 1000);
-// }
-//  let emp1= new Employee("Ghazi Same","Administration","Senior","https://unsplash.com/photos/7YVZYZeITc8");
-// let emp2=new Employee(1001,"Lana Ali","Finance","Senior","https://unsplash.com/photos/Zz5LQe-VSMY");
-// let emp3=new Employee(1002,"Tamara Ayoub","Marketing","Senior","https://unsplash.com/photos/ttSRjiYG_WM");
-// let emp4=new Employee(1003,"Safi Walid","Administration","Mid-Senior","https://unsplash.com/photos/c_GmwfHBDzk");
-// let emp5=new Employee(1004,"Omar Zaid","Development","Senior","https://unsplash.com/photos/mjRwhvqEC0U");
-// let emp6=new Employee(1005,"Rana Saleh","Development","Junior","https://unsplash.com/photos/IF9TK5Uy-KI");
-// let emp7=new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","https://unsplash.com/photos/C8Ta0gwPbQg");
-// emp1.generateUniqueId();
-// emp1.calculateSalary();
-// console.log(emp1.generateUniqueId());
-
-// emp1.render();
-// emp2.calculateSalary();
-// emp2.render();
-// emp3.calculateSalary();
-// emp3.render();
-// emp4.calculateSalary();
-// emp4.render();
-// emp5.calculateSalary();
-// emp5.render();
-// emp6.calculateSalary();
-// emp6.render();
-// emp7.calculateSalary();
-// emp7.render();
